d687204b9e76cc6b8705d7c570aba62ff40c30aa
华子 <puxueqian@whhong.local>
2020-12-24 18:04:01 +0800

    create TODO App

---
+++ server/router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	"go-todo/server/middleware"
+)
+
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/task", middleware.GetAllTask).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/task", middleware.CreateTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/deleteAllTask", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+
+	return router
+}
